6de8e29020603dd425280a5c0ec65ebee0de661c
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-10-23 01:29:10 +0530

    Movies Component

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -2,44 +2,46 @@ import React, { Component } from 'react'
 import { movies } from './getMovies'
 
 export default class Movies extends Component {
-  render() {
+    render() {
 
-    let movieResult = movies.results;
+        let movieResult = movies.results;
 
-    return (
-        
-        <>
-            {
-                movieResult.length == 0 ? 
-                <div>
-                    <div className="spinner-border text-primary" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                    </div>
-                </div> 
-        
-                :
+        return (
 
-                <div>
-                    <h3 style={{fontWeight: 'bold'}} className = 'text-center'>Trending</h3>
-                    <div>
-                        {
-                            movieResult.map((movie) => (
-                                <div className="card movies-card">
-                                    <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}   alt={movie.title} className= 'card-img-top banner-img' />
-                                    <h1 className="card-title banner-title">{movie.original_title}</h1>
-                                    <p  className="card-text banner-text">{movie.overview}</p>
-                                
-                                 </div>
-                            ))
-                        }
-                    </div>
+            <>
+                {
+                    movieResult.length == 0 ?
+                        <div>
+                            <div className="spinner-border text-primary" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div>
 
-                </div>
-                 
-            }
-       
-        </>
+                        :
 
-    )
-  }
+                        <div>
+                            <h3 style={{ fontWeight: 'bold' }} className='text-center'>Trending</h3>
+                            <div className='movies-list'>
+                                {
+                                    movieResult.map((movie) => (
+                                        <div className="card movies-card">
+                                            <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.title} className='card-img-top banner-img' style={{ height: '40vh', width: '20vw' }} />
+                                            <h5 className="card-title movies-title">{movie.original_title}</h5>
+                                            {/* <p className="card-text movies-text">{movie.overview}</p> */}
+                                            <div className="button-wrapper" style={{display: 'flex', width: '100%', justifyContent: 'center'}}>
+                                                <a href="#" className="btn btn-primary movies-button">Add to Favourites</a>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+
+                        </div>
+
+                }
+
+            </>
+
+        )
+    }
 }
