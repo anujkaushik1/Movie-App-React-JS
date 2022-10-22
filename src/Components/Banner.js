52a989480b7619de44244e723a3e55b29e8eb6e1
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-10-22 23:28:57 +0530

    Completed Banner Component

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -16,13 +16,11 @@ export default class Banner extends Component {
                     
                     :
                     
-                    <div className="card">
-                    <img src={`https://image.tmdb.org/t/p/original${movieResult.backdrop_path}`}   alt={movieResult.title} />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
+                    <div className="card banner-card">
+                        <img src={`https://image.tmdb.org/t/p/original${movieResult.backdrop_path}`}   alt={movieResult.title} className= 'card-img-top banner-img' />
+                        <h5 className="card-title banner-title">{movieResult.original_title}</h5>
+                        <p  className="card-text banner-text">{movieResult.overview}</p>
+                        
                     </div>
             }
           
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
   render() {
     return (
-      <div style={{display: 'flex'  }}>
+      <div style={{display: 'flex' }}>
         <h1>Movies App</h1>
         <h2 style={{marginLeft: '2rem', marginTop: '1.5rem'}}>Favourites</h2>
       </div>
