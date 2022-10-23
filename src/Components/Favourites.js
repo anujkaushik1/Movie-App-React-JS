7ecf403ad7d9755429c033357f8796b8d5ebc832
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-10-23 18:37:30 +0530

    Showing All Genres

--- src/Components/Favourites.js
+++ src/Components/Favourites.js
@@ -2,8 +2,18 @@ import React, { Component } from "react";
 import { movies } from "./getMovies";
 
 export default class Favourites extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            genres : []
+        }
+    }
+
     render() {
         const movie = movies.results;
+        let temp = [];
+
         let genreids = {
             28: "Action",
             12: "Adventure",
@@ -25,19 +35,38 @@ export default class Favourites extends Component {
             10752: "War",
             37: "Western",
         };
-        console.log(movie);
+
+        movie.forEach((movieObj)=> {
+            if(!temp.includes(genreids[movieObj.genre_ids[0]])){
+                temp.push(genreids[movieObj.genre_ids[0]]);
+            }
+        })
+
+        temp.unshift("All Geners")
+
+        this.setState({
+            genre : [...temp]
+        })
+
         return (
             <>
                 <div className="main">
                     <div className="row">
                         <div className="col-3">
                             <ul class="list-group favourites-genres">
-                                <li class="list-group-item">All Geners</li>
+
+                                {
+                                    temp.map((genre)=> (
+                                        <li class="list-group-item">{genre}</li>
+                                    ))
+                                }
+
+                                {/* <li class="list-group-item">All Geners</li>
                                 <li class="list-group-item">Action</li>
                                 <li class="list-group-item">Action</li>
                                 <li class="list-group-item">Action</li>
                                 <li class="list-group-item">Action</li>
-                                <li class="list-group-item">Action</li>
+                                <li class="list-group-item">Action</li> */}
                             </ul>
                         </div>
 
@@ -62,56 +91,23 @@ export default class Favourites extends Component {
                                         {
                                             movie.map((movieObj) => (
                                                 <tr>
-                                                    <td>{movieObj.original_title}</td>
-                                                    <td>{genreids[movieObj.genre_ids[1]]}</td>
+                                                    <td><img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} alt={movieObj.title} style={{ width: '5rem' }} />{movieObj.original_title}</td>
+                                                    <td>{genreids[movieObj.genre_ids[0]]}</td>
                                                     <td>{movieObj.popularity}</td>
                                                     <td>{movieObj.vote_average}</td>
                                                     <td><button type="button" class="btn btn-danger">Delete</button></td>
-                                                    
+
                                                 </tr>
                                             ))
                                         }
-
-                                        <tr>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2">Larry the Bird</td>
-                                            <td>@twitter</td>
-                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
-
                             <nav aria-label="Page navigation example">
                                 <ul class="pagination">
-                                    <li class="page-item">
-                                        <a class="page-link" href="#">
-                                            Previous
-                                        </a>
-                                    </li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="#">
-                                            1
-                                        </a>
-                                    </li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="#">
-                                            2
-                                        </a>
-                                    </li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="#">
-                                            3
-                                        </a>
-                                    </li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="#">
-                                            Next
-                                        </a>
-                                    </li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
                                 </ul>
                             </nav>
                         </div>
