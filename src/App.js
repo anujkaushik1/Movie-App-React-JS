36636ba78a3f2fe5d04e023c5e677b8c57537990
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-10-23 23:08:42 +0530

    Implemented Routing Functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar';
-import Banner from './Components/Banner';
-import Movies from './Components/Movies';
-import Favourites from './Components/Favourites';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Banner from "./Components/Banner";
+import Movies from "./Components/Movies";
+import Favourites from "./Components/Favourites";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-      <>
-        <Navbar/>
-        {/* <Banner/>
-        <Movies/> */}
-        <Favourites/>
-      </>
+
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element = {<Movies/>}/>
+        <Route path='/favourites' element={<Favourites/>} />
+
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
